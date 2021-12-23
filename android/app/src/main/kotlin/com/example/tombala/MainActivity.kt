02fcf1998bf6a3ecb1f54440cb55f98d435fb605
sametcilingir <sametcilingir@outlook.com>
2021-12-23 20:37:16 +0300

    ilk commit

---
+++ android/app/src/main/kotlin/com/example/tombala/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tombala
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,98 @@
+
+import 'package:flutter/material.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+//  setupLocator();
+  runApp(MyApp());
+}
+
+// ignore: must_be_immutable
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  // Set default `_initialized` and `_error` state to false
+  bool _initialized = false;
+  bool _error = false;
+
+  // Define an async function to initialize FlutterFire
+  Future<void> initializeFlutterFire() async {
+    /*final NotesViewModel _notesViewModel = locator<NotesViewModel>();
+    try {
+      //flutter downloader burda
+      await FlutterDownloader.initialize(debug: _notesViewModel.debugs);
+    } catch (e) {
+      print("flutter downloader hata   " + e.toString());
+    }*/
+
+    try {
+      // Wait for Firebase to initialize and set `_initialized` state to true
+//      await Firebase.initializeApp();
+      setState(() {
+        _initialized = true;
+        //print("firebase onaylandı");
+      });
+    } catch (e) {
+      // Set `_error` state to true if Firebase initialization fails
+      setState(() {
+        _error = true;
+        print("firebase onaylanmadı   " + e.toString());
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    initializeFlutterFire();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_error) {
+      print("firebase hata");
+
+      //return Center(child: Text('Something Went Wrong'));
+    }
+
+    // Show a loader until FlutterFire is initialized
+    if (!_initialized) {
+      //print("firebase yükleniyor");
+
+      //return Center(child: Text('Loading'));
+    }
+
+    if (_initialized) {
+      //print("firebase onaylandı");
+// Assign listener after the SDK is initialized successfully
+      /*FirebaseAuth.instance.authStateChanges().listen((User? user) {
+        if (user == null) {
+          setState(() {
+          initialRoute ="/auth";
+            
+          });
+        } else {
+          setState(() {
+          initialRoute ="/home";
+            
+          });
+        }
+      });*/
+
+      return MaterialApp(
+        theme: ThemeData(
+          brightness: Brightness.dark,
+        ),
+        darkTheme: ThemeData(brightness: Brightness.dark),
+        themeMode: ThemeMode.dark,
+        debugShowCheckedModeBanner: false,
+        title: 'Tombala',
+        home: Container(),
+      );
+    }
+    return const CircularProgressIndicator();
+  }
+}
